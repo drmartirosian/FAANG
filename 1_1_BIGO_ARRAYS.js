ce4ed0b314f7ee202e1145fb326c8f5582f45990
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2022-02-23 11:08:51 -0600

    BigO

---
+++ 1_1_BIGO_ARRAYS.js
@@ -0,0 +1,11 @@
+// Big O notation
+// O(n) --> Linear Time --> FAIR --> curves up --> operations increase by input 
+// O(1) --> Constant Time --> Flat Line --> num of ops always same, e.g. 1 op every time, 2 ops every time, etc
+/*
+RULE 1: Worst Case
+RULE 2: Remove Constants
+RULE 3: Different Terms
+RULE 4: DROP NON DOMINANTS
+
+
+*/
